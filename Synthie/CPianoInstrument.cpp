f468a0eef900faeee19f8061bd53cc31d536dc1c
asifmasood7 <masoodmu@msu.edu>
2024-10-30 18:27:08 -0400

    Piano Working!

--- Synthie/CPianoInstrument.cpp
+++ Synthie/CPianoInstrument.cpp
@@ -1,118 +1,78 @@
 #include "stdafx.h"
 #include "CPianoInstrument.h"
-#include "Note.h"
+#include "Notes.h"
 
 CPianoInstrument::CPianoInstrument()
-    : m_maxVoices(16), m_attack(0.05), m_release(0.05), m_time(0), m_isPedalPressed(false)
 {
-    m_voices.resize(m_maxVoices);
-    m_frame[0] = m_frame[1] = 0.0;
+    m_duration = 0.1;
 }
 
-CPianoInstrument::CPianoInstrument(int maxVoices)
-    : m_maxVoices(maxVoices), m_attack(0.05), m_release(0.05), m_time(0), m_isPedalPressed(false)
+CPianoInstrument::CPianoInstrument(double bpm)
 {
-    m_voices.resize(m_maxVoices);
-    m_frame[0] = m_frame[1] = 0.0;
+    m_duration = 0.1;
+    m_bpm = bpm;
 }
 
-CPianoInstrument::~CPianoInstrument() {}
-
-void CPianoInstrument::LoadSample(const std::wstring& filename, int midiNote) {
-    if (midiNote >= 21 && midiNote <= 108) {
-        Sample sample;
-        if (sample.Load(filename)) {
-            m_samples[midiNote] = sample;
-        }
-    }
+CPianoInstrument::~CPianoInstrument()
+{
 }
 
-void CPianoInstrument::Start() {
+void CPianoInstrument::Start()
+{
+    m_sinewave.SetSampleRate(GetSampleRate());
+    m_sinewave.Start();
+    m_ar.SetSource(&m_sinewave);
+    m_ar.SetSampleRate(GetSampleRate());
+    m_ar.SetAttack(0.01); // Attack time in seconds
+    m_ar.SetRelease(m_duration); // Release over the duration
+    m_ar.SetDuration(m_duration); // Total duration equals release time
+    m_ar.Start();
     m_time = 0;
-    m_frame[0] = m_frame[1] = 0.0;
-    if (m_currentNote) {
-        StartNote(m_currentNote->GetPitch(), m_currentNote->GetVelocity());
-    }
 }
 
-bool CPianoInstrument::Generate() {
-    // Reset m_frame for stereo output
-    m_frame[0] = 0.0;
-    m_frame[1] = 0.0;
+bool CPianoInstrument::Generate()
+{
+    bool valid = m_ar.Generate();
 
-    // Generate samples for all active voices
-    for (auto& voice : m_voices) {
-        if (voice.active) {
-            double sampleValue = voice.sample.Generate();
-            m_frame[0] += ApplyDynamics(sampleValue, voice.velocity);
-            m_frame[1] += ApplyDynamics(sampleValue, voice.velocity);
-            voice.duration -= m_samplePeriod;
-            if (voice.duration <= 0) voice.active = false;
+    // Generate the piano sound
+    m_frame[0] = m_ar.Frame(0);
+    m_frame[1] = m_ar.Frame(1);
+
+    // Advance time
+    m_time += GetSamplePeriod();
+
+    // Check if the note duration is complete
+    return valid;
+}
+
+void CPianoInstrument::SetNote(CNote* note)
+{
+    // Retrieve attributes from the note
+    CComPtr<IXMLDOMNamedNodeMap> attributes;
+    note->Node()->get_attributes(&attributes);
+    long len;
+    attributes->get_length(&len);
+
+    for (int i = 0; i < len; i++)
+    {
+        CComPtr<IXMLDOMNode> attrib;
+        attributes->get_item(i, &attrib);
+
+        CComBSTR name;
+        attrib->get_nodeName(&name);
+
+        CComVariant value;
+        attrib->get_nodeValue(&value);
+
+        if (name == L"duration")
+        {
+            value.ChangeType(VT_R8);
+            m_duration = value.dblVal;
+            m_ar.SetDuration(m_duration);
+        }
+        else if (name == L"note")
+        {
+            SetFreq(NoteToFrequency(value.bstrVal));
         }
     }
-
-    // Apply envelope to the frame
-    double envelopeValue = ApplyEnvelope(m_time);
-    m_frame[0] *= envelopeValue;
-    m_frame[1] *= envelopeValue;
-
-    // Apply pedal noise if pressed
-    if (m_isPedalPressed) GeneratePedalNoise(true);
-
-    // Update time for envelope progression
-    m_time += m_samplePeriod;
-    return IsEnvelopeActive(m_time);
-}
-
-void CPianoInstrument::SetNote(CNote* note) {
-    m_currentNote = note;
-    if (m_currentNote) {
-        StartNote(m_currentNote->GetPitch(), m_currentNote->GetVelocity());
-    }
-}
-
-void CPianoInstrument::SetPedal(bool pressed) {
-    m_isPedalPressed = pressed;
-    GeneratePedalNoise(pressed);
-}
-
-void CPianoInstrument::StartNote(int midiNote, double velocity) {
-    // Check if the MIDI note is mapped to a sample
-    if (m_samples.find(midiNote) != m_samples.end()) {
-        Sample& sample = m_samples[midiNote];
-        AddVoice(sample, velocity, 1.0);  // Adding the note with a default duration of 1.0 seconds
-    }
-}
-
-void CPianoInstrument::AddVoice(Sample& sample, double velocity, double duration) {
-    for (auto& voice : m_voices) {
-        if (!voice.active) {
-            voice.sample = sample;
-            voice.velocity = velocity;
-            voice.duration = duration;
-            voice.active = true;
-            break;
-        }
-    }
-}
-
-double CPianoInstrument::ApplyDynamics(double input, double velocity) {
-    double minLevel = 0.0, maxLevel = 1.0;
-    return (input * velocity < minLevel) ? minLevel : (input * velocity > maxLevel) ? maxLevel : input * velocity;
-}
-
-double CPianoInstrument::ApplyEnvelope(double time) {
-    if (time < m_attack) return time / m_attack;
-    else if (time > 1.0 - m_release) return (1.0 - time) / m_release;
-    return 1.0;
-}
-
-bool CPianoInstrument::IsEnvelopeActive(double time) const {
-    return time < 1.0;
-}
-
-void CPianoInstrument::GeneratePedalNoise(bool isPressing) {
-    double noise = isPressing ? m_pedalDownNoise.Generate() : m_pedalUpNoise.Generate();
-    m_frame[0] += noise;
-    m_frame[1] += noise;
 }
--- Synthie/CPianoInstrument.h
+++ Synthie/CPianoInstrument.h
@@ -1,57 +1,28 @@
 #pragma once
 #include "Instrument.h"
-#include "Sample.h"
-#include <map>
-#include <vector>
+#include "SineWave.h"
+#include "AR.h"
 
 class CPianoInstrument : public CInstrument
 {
 public:
     CPianoInstrument();
-    CPianoInstrument(int maxVoices);
+    CPianoInstrument(double bpm);
     virtual ~CPianoInstrument();
 
-    virtual void Start() override;
-    virtual bool Generate() override;
-    virtual void SetNote(CNote* note) override;
+    const double NUM_SECS_IN_MINUTE = 60.0;
+    virtual void Start();
+    virtual bool Generate();
 
-    void SetPedal(bool pressed);
-    void LoadSample(const std::wstring& filename, int midiNote);
+    void SetFreq(double f) { m_sinewave.SetFreq(f); }
+    void SetAmplitude(double a) { m_sinewave.SetAmplitude(a); }
+    void SetDuration(double d) { m_duration = d; }
+    virtual void SetNote(CNote* note);
 
 private:
-    struct Voice {
-        Sample sample;
-        double velocity;
-        double duration;
-        bool active;
-    };
-
-    // Piano-related attributes
-    std::map<int, Sample> m_samples;  // MIDI note to Sample mapping
-    std::vector<Voice> m_voices;      // Active voices for polyphony
-    int m_maxVoices;
-
-    // Envelope and dynamics
-    double m_attack;
-    double m_release;
+    CSineWave m_sinewave;
+    CAR m_ar;
+    double m_duration;
     double m_time;
-
-    // Pedal and effects
-    bool m_isPedalPressed;
-    Sample m_pedalDownNoise;
-    Sample m_pedalUpNoise;
-
-    // Frame buffer for stereo output
-    double m_frame[2];
-
-    // Pointer to the current note
-    CNote* m_currentNote;
-
-    // Internal helper methods
-    void StartNote(int midiNote, double velocity);
-    void AddVoice(Sample& sample, double velocity, double duration);
-    double ApplyDynamics(double input, double velocity);
-    double ApplyEnvelope(double time);
-    bool IsEnvelopeActive(double time) const;
-    void GeneratePedalNoise(bool isPressing);
+    double m_bpm;
 };
